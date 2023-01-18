0e5e1358a7601bde1a0cc04738d6286541dee0ad
Ramona <gherasim.ramonaa@yahoo.ro>
2023-01-18 01:30:26 +0000

    "Update suites to include Verification tests "

--- suites.py
+++ suites.py
@@ -1,5 +1,6 @@
 import unittest
 from Alerts import Alerts
+from tema9_Verificatori import Login
 # din numele fisierului import numele clasei
 # daca fisierul din care importam e intr-un alt folder, se scrie numele folderului inainte
 
@@ -10,8 +11,8 @@ import HtmlTestRunner
 class TestSuite(unittest.TestCase):
     def test_suite(self):
         test_derulat = unittest.TestSuite()
-        test_derulat.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(Alerts))
-        runner = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_title="My First Report", report_name="My First Report Name")
+        test_derulat.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(Alerts), unittest.defaultTestLoader.loadTestsFromTestCase(Login)])
+        runner = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_title="Tests Report", report_name="Alerts and Login Suite")
         runner.run(test_derulat)
         # Combine reports ne genereaza un singur raport cu toate rezultatele pentru toate testele
 
