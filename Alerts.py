7871ad159bbde9edcf827a0fe810738ed79b4d60
Ramona <gherasim.ramonaa@yahoo.ro>
2023-01-18 19:53:45 +0000

    "Update Alerts and Suites files after course12"

--- Alerts.py
+++ Alerts.py
@@ -14,16 +14,17 @@
 
 # import unittest  # importa tot modulul. Aici clasa mosteneste din unittest.TestCase
 # Sau se poate importa doar pachetul:
+import unittest
 from unittest import TestCase  # in acest caz clasa mosteneste direct din TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 class Alerts(TestCase):
-    alert_button = (By.XPATH, '//*[text()="Click for JS Alert"]')
-    confirm_button = (By.XPATH, '//*[text()="Click for JS Confirm"]')
-    prompt_button = (By.XPATH, '//*[text()="Click for JS Prompt"]')
-    alert_msg = (By.ID, "result")
+    ALERT_BUTTON = (By.XPATH, '//*[text()="Click for JS Alert"]')
+    CONFIRM_BUTTON = (By.XPATH, '//*[text()="Click for JS Confirm"]')
+    PROMPT_BUTTON = (By.XPATH, '//*[text()="Click for JS Prompt"]')
+    ALERT_MSG = (By.ID, "result")
 
     # Definirea metodei de setUp
     def setUp(self) -> None:
@@ -37,15 +38,64 @@ class Alerts(TestCase):
         self.chrome.quit()
 
     def test_alert_accept(self):
-        self.chrome.find_element(*self.alert_button).click()
+        self.chrome.find_element(*self.ALERT_BUTTON).click()
         js_alert = self.chrome.switch_to.alert
         js_alert.accept()  # built-in method care accepta alerta e.g. da click pe buton
-        actual_alert_msg = self.chrome.find_element(*self.alert_msg).text
+        actual_alert_msg = self.chrome.find_element(*self.ALERT_MSG).text
         expected_alert_msg = "You successfully clicked an alert"
-        assert actual_alert_msg == expected_alert_msg, f"Error: {expected_alert_msg}, actual {actual_alert_msg} "
+        assert actual_alert_msg == expected_alert_msg, f"Error: {expected_alert_msg}, actual {actual_alert_msg}"
 
-        # self.chrome.find_element(*self.confirm_button).click()
-        # self.chrome.find_element(*self.prompt_button).click()
+    def test_confirm_accept(self):
+        self.chrome.find_element(*self.CONFIRM_BUTTON).click()
+        js_confirm = self.chrome.switch_to.alert
+        js_confirm.accept()
+        actual_confirm_msg = self.chrome.find_element(*self.ALERT_MSG).text
+        expected_confirm_msg = "You clicked: Ok"
+        assert actual_confirm_msg == expected_confirm_msg, f"Error: {expected_confirm_msg}, actual {actual_confirm_msg}"
+
+    def test_confirm_cancel(self):
+        self.chrome.find_element(*self.CONFIRM_BUTTON).click()
+        js_confirm = self.chrome.switch_to.alert
+        js_confirm.dismiss()
+        actual_confirm_msg = self.chrome.find_element(*self.ALERT_MSG).text
+        expected_confirm_msg = "You clicked: Cancel"
+        assert actual_confirm_msg == expected_confirm_msg, f"Error: {expected_confirm_msg}, actual {expected_confirm_msg}"
+
+    @unittest.skip   # Posibilitate de a da skip la un test, folosind decoratori
+    def test_prompt_no_text_accept(self):
+        self.chrome.find_element(*self.PROMPT_BUTTON).click()
+        js_prompt = self.chrome.switch_to.alert
+        js_prompt.accept()
+        actual_prompt_msg = self.chrome.find_element(*self.ALERT_MSG).text
+        expected_prompt_msg = "You entered:"
+        assert actual_prompt_msg == expected_prompt_msg, f"Error: {actual_prompt_msg}, actual {expected_prompt_msg}"
+
+    @unittest.skip
+    def test_prompt_no_text_dismiss(self):
+        self.chrome.find_element(*self.PROMPT_BUTTON).click()
+        js_prompt = self.chrome.switch_to.alert
+        js_prompt.dismiss()
+        actual_prompt_msg = self.chrome.find_element(*self.ALERT_MSG).text
+        expected_prompt_msg = "You entered: null"
+        assert actual_prompt_msg == expected_prompt_msg, f"Error: {actual_prompt_msg}, actual {expected_prompt_msg}"
+
+    def test_prompt_text_accept(self):
+        self.chrome.find_element(*self.PROMPT_BUTTON).click()
+        js_prompt = self.chrome.switch_to.alert
+        js_prompt.send_keys("Hello!")
+        js_prompt.accept()
+        actual_prompt_msg = self.chrome.find_element(*self.ALERT_MSG).text
+        expected_prompt_msg = "You entered: Hello!"
+        assert actual_prompt_msg == expected_prompt_msg, f"Error: {actual_prompt_msg}, actual {expected_prompt_msg}"
+
+    def test_prompt_text_dismiss(self):
+        self.chrome.find_element(*self.PROMPT_BUTTON).click()
+        js_prompt = self.chrome.switch_to.alert
+        js_prompt.send_keys("Hello!")
+        js_prompt.dismiss()
+        actual_prompt_msg = self.chrome.find_element(*self.ALERT_MSG).text
+        expected_prompt_msg = "You entered: null"
+        assert actual_prompt_msg == expected_prompt_msg, f"Error: {actual_prompt_msg}, actual {expected_prompt_msg}"
 
 
 
--- suites.py
+++ suites.py
@@ -1,6 +1,10 @@
 import unittest
 from Alerts import Alerts
 from tema9_Verificatori import Login
+from Keys import Key
+from Context_menu import ContextMenu
+from Firefox_auth import Firefox
+
 # din numele fisierului import numele clasei
 # daca fisierul din care importam e intr-un alt folder, se scrie numele folderului inainte
 
@@ -11,8 +15,8 @@ import HtmlTestRunner
 class TestSuite(unittest.TestCase):
     def test_suite(self):
         test_derulat = unittest.TestSuite()
-        test_derulat.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(Alerts), unittest.defaultTestLoader.loadTestsFromTestCase(Login)])
-        runner = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_title="Tests Report", report_name="Alerts and Login Suite")
+        test_derulat.addTests([unittest.defaultTestLoader.loadTestsFromTestCase(Alerts), unittest.defaultTestLoader.loadTestsFromTestCase(Login), unittest.defaultTestLoader.loadTestsFromTestCase(Key), unittest.defaultTestLoader.loadTestsFromTestCase(Firefox), unittest.defaultTestLoader.loadTestsFromTestCase(ContextMenu)])
+        runner = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_title="Tests Report3", report_name="Alerts, Login, Key, ContextMenu and Firefox Suite")
         runner.run(test_derulat)
         # Combine reports ne genereaza un singur raport cu toate rezultatele pentru toate testele
 
